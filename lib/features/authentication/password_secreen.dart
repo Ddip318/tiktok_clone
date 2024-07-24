e01e2987655e8dd6a72cfea8e76a26e10695155c
Ddip318 <wadrt175t@gmail.com>
2024-07-24 19:59:44 +0900

    Email reg

---
+++ lib/features/authentication/password_secreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PasswordSecreen extends StatelessWidget {
+  const PasswordSecreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
